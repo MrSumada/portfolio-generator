f772a754325c2dd8c1e930aa5d5c2e8578854fd3
MrSumada <ricksamericana.com>
2022-02-07 21:47:05 -0500

    updated template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const inquirer = require("inquirer");
-// const fs = require('fs');
-// const generatePage = require("./src/page-template.js");
+const fs = require('fs');
+const generatePage = require("./src/page-template.js");
 
 
 const promptUser = () => {
@@ -128,18 +128,17 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+
+        const pageHTML = generatePage(portfolioData);
+        
+        fs.writeFile("./index.html", pageHTML, err => {
+            if (err) throw new Error(err);
+        })
     });
 
 
 
-// const pageHTML = generatePage(username, github);
 
-// fs.writeFile("./index.html", pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 
 
 
